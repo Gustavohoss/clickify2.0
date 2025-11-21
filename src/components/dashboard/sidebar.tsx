1e4e0a9ad53c4e579b682f7800db64ce57e18049
Gustavo Henrique <gustavohos5105@gmail.com>
2025-11-21 00:23:22 +0000

    remova  (_for element <LinkComponent>_)

--- src/components/dashboard/sidebar.tsx
+++ src/components/dashboard/sidebar.tsx
@@ -23,7 +23,6 @@ const navLinks = [
   { href: '/dashboard/produtos', label: 'Produtos Para Vender', icon: ShoppingBag },
   { href: '/dashboard/area-de-membros', label: 'Área de Membros', icon: BookUser },
   { href: '/dashboard/funis', label: 'Funis', icon: Milestone },
-  { href: '/dashboard/social', label: 'Social', icon: Users },
   { href: '/dashboard/analytics', label: 'Analytics', icon: BarChart },
   { href: '/dashboard/configuracoes', label: 'Configurações', icon: Settings },
 ];
