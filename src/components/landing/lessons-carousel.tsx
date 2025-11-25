019641adfe7a6e9a5c7164e0ba6f69a21fc7925a
Gustavo Henrique <gustavohos5105@gmail.com>
2025-11-25 05:04:28 +0000

    Nessa parte deixe apenas esses 3:
    https://s3.typebot.io/public/workspac

--- src/components/landing/lessons-carousel.tsx
+++ src/components/landing/lessons-carousel.tsx
@@ -2,19 +2,27 @@
 
 import React from 'react';
 import Image from 'next/image';
-import { PlaceHolderImages } from '@/lib/placeholder-images';
 import useEmblaCarousel from 'embla-carousel-react';
 import Autoplay from 'embla-carousel-autoplay';
 import { cn } from '@/lib/utils';
 
 const lessons = [
-  PlaceHolderImages.find(p => p.id === 'lesson-card-1'),
-  PlaceHolderImages.find(p => p.id === 'lesson-card-2'),
-  PlaceHolderImages.find(p => p.id === 'lesson-card-3'),
-  PlaceHolderImages.find(p => p.id === 'lesson-card-4'),
-  PlaceHolderImages.find(p => p.id === 'lesson-card-5'),
-  PlaceHolderImages.find(p => p.id === 'lesson-card-6'),
-].filter(Boolean) as any[];
+  {
+    id: 'lesson-1',
+    imageUrl: 'https://s3.typebot.io/public/workspaces/cm8gbxl5b000ba3ncy4y16grd/typebots/cmi0sldz2000djl043bd6dtvj/blocks/y510lieie4op8pdhz5o07zj8?v=1764037761049',
+    description: 'Aula 1',
+  },
+  {
+    id: 'lesson-2',
+    imageUrl: 'https://s3.typebot.io/public/workspaces/cm8gbxl5b000ba3ncy4y16grd/typebots/cmi0sldz2000djl043bd6dtvj/blocks/e9v9vopox1y5qctk6ztvj4mw?v=1764038145428',
+    description: 'Aula 2',
+  },
+  {
+    id: 'lesson-3',
+    imageUrl: 'https://s3.typebot.io/public/workspaces/cm8gbxl5b000ba3ncy4y16grd/typebots/cmi0sldz2000djl043bd6dtvj/blocks/rtykaylwwnetzpo5wcb5o4p5?v=1764046946432',
+    description: 'Aula 3',
+  },
+];
 
 export const LessonsCarousel = () => {
   const [emblaRef] = useEmblaCarousel({ loop: true, align: 'start' }, [
@@ -29,7 +37,7 @@ export const LessonsCarousel = () => {
         </h1>
         <div className="overflow-hidden" ref={emblaRef}>
           <div className="flex -ml-4">
-            {[...lessons, ...lessons].map((lesson, index) => (
+            {[...lessons, ...lessons, ...lessons, ...lessons].map((lesson, index) => (
               <div key={`${lesson.id}-${index}`} className="relative flex-[0_0_280px] pl-4">
                 <Image
                   src={lesson.imageUrl}
