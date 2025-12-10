b0f0804859d4ad0e09431270580161c01e5033be
Gustavo Henrique <gustavohos5105@gmail.com>
2025-12-10 16:58:30 +0000

    Crie um 6.0 com deixe o lugar dos links vasios, pois eu que vou colocar

--- src/app/6.0/page.tsx
+++ src/app/6.0/page.tsx
@@ -1,4 +1,4 @@
-// GABRIEL
+//AFILIADO
 'use client';
 
 import { Button } from "@/components/ui/button";
@@ -136,7 +136,7 @@ export default function Home() {
                         "Suporte básico"
                     ]}
                     isFeatured={false}
-                    checkoutUrl="https://pay.cakto.com.br/xy27qg8?affiliate=BqV5o2ug"
+                    checkoutUrl="#"
                 />
                 <NewPricingCard
                     title="Plano Ilimitado"
@@ -151,7 +151,7 @@ export default function Home() {
                         "Todas as atualizações incluídas"
                     ]}
                     isFeatured={true}
-                    checkoutUrl="https://pay.cakto.com.br/37k4xrc_658280?affiliate=BqV5o2ug"
+                    checkoutUrl="#"
                 />
             </div>
         </div>
